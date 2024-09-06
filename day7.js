b63779f60d078e996669b6be4b021a73fcd0e008
Kahmii <oladejokhaleed9@gmail.com>
2024-09-06 06:28:12 +0100

    th

--- day7.js
+++ day7.js
@@ -13,7 +13,23 @@ function playGame() {
     else if (player < numberToGuess) {
         result.textContent = "A little bit higher almost there!! try again"
     }
+    else if (player > 10) {
+        result.textContent = "wrong input"
+    }
     else{
         result.textContent = "A little bit lower almost there!! try again"
     }
+}
+
+
+let cont = document.getElementById("t0List")
+let butt = document.getElementById("add")
+let put = document.getElementById("getName")
+
+function addListItem() {
+   let list = put.value;
+   let li = document.createElement("li")
+   li.innerText = list;
+   cont.appendChild(li);
+   put.value = '';
 }
