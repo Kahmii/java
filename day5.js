30a804abcda8c7a7541d8b6e39ee792998a3c414
Kahmii <oladejokhaleed9@gmail.com>
2024-09-18 09:53:52 +0100

    gg

--- day5.js
+++ day5.js
@@ -1,14 +1,42 @@
-let time = Number(prompt("What is the time in 24hours"))
+// let time = Number(prompt("What is the time in 24hours"))
 
-if (time < 12){
-    alert(`it is ${time}good moring`);
+// if (time < 12){
+//     alert(`it is ${time}good moring`);
+// }
+// else if (time >= 12) {
+//     alert(`it is ${time} Good afternoon`)
+// }
+// else if (time < 24) {
+//     alert(`invalid hour fam `)
+// }
+// else{
+//     alert(`Wrong Hour`)
+
+// }
+
+let getCorrectDate = new Date ()
+
+console.log(getCorrectDate.getFullYear());
+console.log(getCorrectDate.getMonth()+1);
+console.log(getCorrectDate.getDate());
+console.log(getCorrectDate.getDay()+1);
+console.log(getCorrectDate.getMinutes());
+console.log(getCorrectDate.getSeconds());
+console.log(getCorrectDate.getMilliseconds());
+
+
+let greeting = document.getElementById("greet")
+let yourname = prompt("what is your name")
+
+if(getCorrectDate.getHours() <= 12){
+   greeting.innerText = `Good afternoon ${yourname.toUpperCase()}`
 }
-else if (time >= 12) {
-    alert(`it is ${time} Good afternoon`)
+else if (getCorrectDate.getHours()<=12) {
+   greeting.innerText = `Good morning ${yourname.toUpperCase()}`
 }
-else if (time < 24) {
-    alert(`invalid hour fam `)
+else if(getCorrectDate.getHours()>=16){
+   greeting.innerText = `Good evening ${yourname.toUpperCase()}`
 }
 else{
-    alert(`Wrong Hour`)
+   greeting.innerText= `Good day ${yourname.toUpperCase()}`
 }
--- event.js
+++ event.js
@@ -84,22 +84,22 @@
 //  } while (p<20);
 
 
-  let countires = ["Nigeria","Ghana", "congo","canada","togo"]
+
 //  for (let g = 0; g < 5; g++) {
 //     console.log(countires[g]);
-    
+
 //  }
 
 //  let mystring = "javascript is sweet"
 //  for (let y = 0; y < mystring.length; y++) {
 //     console.log(mystring[y])
-    
+
 //  }
 // let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 //  let sum = 0
 //  for (let u = 0; u < number.length; u++) {
 //     sum += number[u]
-    
+
 //  }
 //  console.log(sum);
 
@@ -109,7 +109,7 @@
 //     su += number[m]
 //     ul.innerHTML += `<li>${su}</li>`
 //     console.log(su)
-    
+
 //  }
 // console.log(su)
 
@@ -120,14 +120,144 @@
 //    newarr.push(countires[t])
 //    ul.innerHTML += `<li>${newarr[t]}</li>`
 // }
-let newArr = []
-let newnew = document.getElementById("newnew")
-for(const country of countires){
-   newnew.innerHTML+=`<li>${country}</li>`
+// let newArr = []
+// let newnew = document.getElementById("newnew")
+// for(const country of countires){
+//    newnew.innerHTML+=`<li>${country}</li>`
+// }
+
+
+// countires.forEach((index)=> {
+//    console.log(index)
+// })
+//creat a password generator using 15character length
+
+//break and continue
+//stopp and start
+
+
+for (let d = 0; d < 20; d++) {
+   if (d == 15) {
+      continue
+   }
+   console.log(d)
 }
 
+//Array method
+//.join
+//object method
+let countires = ["Nigeria", "Ghana", "congo", "canada", "togo"]
 
-countires.forEach((index)=> {
-   console.log(index)
+let person = {
+   name: "khaleed",
+   agge: 30,
+   class: "100 level",
+
+}
+let aboutme = `my name is ${person.name}`
+console.log(aboutme);
+
+let peple = {
+   canada: {
+      capital: "scarborg",
+      course: "web develop",
+      age: 30,
+      class: "100 level"
+   },
+   london: {
+      capital: "Abuja",
+      course: "web develop",
+      age: 30,
+      class: "100 level",
+   },
+
+   togo: {
+      capital: "Abuja",
+      course: "web develop",
+      age: 30,
+      class: "100 level"
+   },
+} 
+    
+let about = `the name of this and the capital is ${peple.canada.capital}`
+console.log(about)
+
+//object methods
+
+
+let getkeys = Object.keys(peple)
+let getvalues = Object.values(peple)
+
+getkeys.forEach((countrycap)=>{
+   let details = `the name of the county is ${countrycap} `
+   console.log(details)
 })
-//creat a password generator using 15character length
+
+let peple2 = [
+  {
+      capital: "umaya",
+      course: "web develop",
+      age: 30,
+      class: "100 level"
+   },
+   {
+      capital: "abear",
+      course: "web develop",
+      age: 30,
+      class: "100 level",
+   },
+  {
+      capital: "togo",
+      course: "web develop",
+      age: 30,
+      class: "100 level"
+   },
+] 
+
+peple2.forEach(values => {
+   values.year = "2024"
+   console.log(values)
+});
+
+
+
+//assign
+let me = {}
+Object.assign(me, peple.canada)
+console.log(me)
+
+
+//how to style dynamically 
+ let getH1 =  document.querySelector("h1")
+
+let myStyle = {
+   background: 'red',
+   padding: '20px',
+   borderRadius: '10px',
+   border: '2px solid gold',
+}
+
+Object.assign(getH1.style, myStyle)
+
+//date object and  its method
+
+let getCorrectDate = new Date ()
+
+console.log(getCorrectDate.getFullYear());
+console.log(getCorrectDate.getMonth()+1);
+console.log(getCorrectDate.getDate());
+console.log(getCorrectDate.getDay()+1);
+console.log(getCorrectDate.getMinutes());
+console.log(getCorrectDate.getSeconds());
+console.log(getCorrectDate.getMilliseconds());
+//spread opertor under object
+
+let greeting = document.getElementById("greet")
+let yourname = prompt("what is your name")
+
+if(getCorrectDate.getHours()>= 12){
+   greeting.innerText = `Good afternoon ${yourname.toUpperCase()}`
+}
+else{
+   greeting.innerText= `Good day ${yourname.toUpperCase()}`
+}
